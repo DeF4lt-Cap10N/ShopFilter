0546cf6fbfa04a247185551dea48071ebc67acdb
akash1675 <captainakash1675@gmail.com>
2025-03-08 17:31:36 +0530

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Nav from "./Navigation/Nav"
+import Product from "./Product/Product"
+import Recommended from "./Recommended/Recommended"
 
 function App() {
 
   return (
     <>
       <div>
-        Hello 
+       <Nav/>
+       <Product/>
+       <Recommended/>
       </div>
     </>
   )
