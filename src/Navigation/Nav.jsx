887fd90d31b0517296e301f49de735a2f9617c85
akash1675 <captainakash1675@gmail.com>
2025-03-08 18:33:45 +0530

    add product&nav

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { FiHeart } from 'react-icons/fi';
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from 'react-icons/ai';
+
+
 import './Nav.css'
+
 
 const Nav = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <div className='nav-container'>
+        <input
+         type="text"
+          name="" 
+          id=""
+          className='search-input'
+          placeholder='Enter you search shoes'
+          />
+      </div>
+
+      <div className="pofile-container">
+        <a href="#">
+          <FiHeart className = "nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons"/>
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className= "nav-icons"/>
+        </a>
+        
+      </div>
+    </nav>
   )
 }
 
