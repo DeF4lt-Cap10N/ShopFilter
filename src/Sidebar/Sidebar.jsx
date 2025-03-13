94894aa99cf7599108f92f779513c5965bc1498a
akash1675 <captainakash1675@gmail.com>
2025-03-13 14:56:42 +0530

    add sidebar

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Category from './Category/Category'
 import Price from './Price/Price'
 import Colors from './Colors/Color'
+import './sidebar.css'
 
 const Sidebar = () => {
   return (
     <>
     <section className="sidebar">
       <div className="logo-container">
-         <h1>cart</h1>
+         <h1 className='sidebar-title'>cart</h1>
       </div>
-      
       <Category/>
       <Price/>
       <Colors/>
