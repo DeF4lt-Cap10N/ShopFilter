f949ccb3b4bc53d00c6a19b1890309d061d57128
Rahul sharma <captainakash1675@gmail.com>
2025-03-16 22:36:33 +0530

    efactor
    r#

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,34 +1,17 @@
 import React from 'react'
+import Input from '../../components/Input'
 import './Price.css'
 
 const Price = () => {
   return (
     <div className='ml'>
       <h2 className="sidebar-title price-title">Price</h2>
-
-      <label className="sidebar-label-container">
-          <input type="radio" name='test2' />
-          <span className="checkmark"></span>$0 - $50
-        </label>
-
-      <label className="sidebar-label-container">
-          <input type="radio" name='test2' />
-          <span className="checkmark"></span>$50 - $100
-        </label>
-
-      <label className="sidebar-label-container">
-          <input type="radio" name='test2' />
-          <span className="checkmark"></span>$100 - $150
-        </label>
-
-      <label className="sidebar-label-container">
-          <input type="radio" name='test2' />
-          <span className="checkmark"></span>$150 - $500
-        </label>
-      <label className="sidebar-label-container">
-          <input type="radio" name='test2' />
-          <span className="checkmark"></span>over $500
-        </label>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+    
     </div>
   )
 }
