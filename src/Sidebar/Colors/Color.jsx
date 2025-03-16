c8a363f3fb4ea3606f2604cc322c63d0f44f43e6
Rahul Sharma <captain@gmail.com>
2025-03-16 21:23:53 +0530

    add sidebar color sec.

--- src/Sidebar/Colors/Color.jsx
+++ src/Sidebar/Colors/Color.jsx
@@ -4,7 +4,34 @@ import './Color.css'
 const Color = () => {
   return (
     <div>
-      colors
+      <label className="sidebar-label-container color-title">
+        <input type="radio" name='test2' />
+        <span className="checkmark"></span>All
+      </label>
+
+      <label className="sidebar-label-container">
+        <input type="radio" name='test2' />
+        <span className="checkmark"></span>Black
+      </label>
+
+      <label className="sidebar-label-container">
+        <input type="radio" name='test2' />
+        <span className="checkmark"></span>Blue
+      </label>
+
+      <label className="sidebar-label-container">
+        <input type="radio" name='test2' />
+        <span className="checkmark"></span>Red
+      </label>
+
+      <label className="sidebar-label-container">
+        <input type="radio" name='test2' />
+        <span className="checkmark"></span>Green
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name='test2' />
+        <span className="checkmark"></span>Yellow
+      </label>
     </div>
   )
 }
